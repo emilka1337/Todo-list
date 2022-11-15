2005cf088d13784b6a3866f4888058fc958aab4b
Emil <maxmudov2000@gmail.com>
2022-11-15 21:26:45 +0400

    completed added

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ document.querySelector('.main-form').addEventListener("submit", function (e) {  
     e.preventDefault();
     let newTodoText = document.querySelector('#newTodo').value;
     document.querySelector('#newTodo').value = "";                                 // Очистка поля ввода
-    TODOS.push(newTodoText);
+    TODOS.push({
+        text: newTodoText,
+        completed: false
+    });
     generateTodoList();                                                            // Запуск генерации ToDo-list'a
 });
 
@@ -31,9 +34,15 @@ function generateTodoList() {                              // Генерация
     TODOS.forEach((todo, index) => {                       // Генерация списка по массиву
         let li = document.createElement("li");
         li.className = "todo-list-item";
+        todo.completed ? li.classList.add("completed") : null;
+
+        li.addEventListener("click", function() {
+            TODOS[index].completed = !TODOS[index].completed;
+            this.classList.toggle("completed");
+        })
 
         let p = document.createElement("p");
-        p.innerHTML = todo;
+        p.innerHTML = todo.text;
 
         let deleteButton = document.createElement("button");
         deleteButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"class="bi bi-x-lg" viewBox = "0 0 16 16" ><path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" /></svg >';
@@ -41,7 +50,7 @@ function generateTodoList() {                              // Генерация
         deleteButton.addEventListener("click", function () {    // Обработчик события удаления одного элемента
             TODOS.splice(index, 1);                             // Безопасное удаление элемента из массива
             generateTodoList();                                 // Генерация списка заново после удаления
-        })
+        });
 
         li.append(p, deleteButton);
         document.querySelector('.todo-list').append(li);
